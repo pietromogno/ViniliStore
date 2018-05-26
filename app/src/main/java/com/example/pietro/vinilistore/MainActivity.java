dd6e8cf530e9a6c135c9bde059ba3aae9de84a5d
pietromogno <pietro.mogno98@gmail.com>
2018-05-26 20:51:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pietro/vinilistore/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pietro.vinilistore;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button btnLogin , btnRegistrati;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegistrati = (Button) findViewById(R.id.btnRegistrati);
+        btnRegistrati.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent registrazione = new Intent(view.getContext(),Registrazione.class);
+            startActivity(registrazione);
+        }
+    });
+
+}}
---
+++ app/src/main/java/com/example/pietro/vinilistore/Registrazione.java
@@ -0,0 +1,13 @@
+package com.example.pietro.vinilistore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registrazione extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrazione);
+    }
+}
