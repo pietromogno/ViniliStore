58944534b7f649aeda478a634147824b6528179a
pietromogno <pietro.mogno98@gmail.com>
2018-06-02 14:27:21 +0200

    terzo commit

---
+++ app/src/main/java/com/example/pietro/vinilistore/HTTPDataHandler.java
@@ -0,0 +1,130 @@
+package com.example.pietro.vinilistore;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class HTTPDataHandler {
+
+    static String stream = null;
+
+    public HTTPDataHandler(){}
+
+    public void GetHTTPData(String urlString){
+        try{
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+
+            //controllo stato connessione
+            if(urlConnection.getResponseCode() == 200){
+                //codice risposta = 200 ===> connessione ok
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                //lettura del BufferedInputStream
+                BufferedReader r = new BufferedReader(new InputStreamReader(in));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while((line=r.readLine()) != null)
+                    sb.append(line);
+                stream = sb.toString();
+                urlConnection.disconnect();
+            }
+            else{
+
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void PostHTTPData(String urlString,String json){
+        try{
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoOutput(true);
+
+            byte[] out = json.getBytes(StandardCharsets.UTF_8);
+            int lenght = out.length;
+
+            urlConnection.setFixedLengthStreamingMode(lenght);
+            urlConnection.setRequestProperty("Content-Type","application/json; charset=UTP-8");
+            urlConnection.connect();
+            try(OutputStream os = urlConnection.getOutputStream())
+            {
+                os.write(out);
+            }
+            InputStream response = urlConnection.getInputStream();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void PutHTTPData (String urlString,String newValue){
+        try{
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+
+            urlConnection.setRequestMethod("PUT");
+            urlConnection.setDoOutput(true);
+
+            byte[] out = newValue.getBytes(StandardCharsets.UTF_8);
+            int lenght = out.length;
+
+            urlConnection.setFixedLengthStreamingMode(lenght);
+            urlConnection.setRequestProperty("Content-Type","application/json; charset=UTP-8");
+            urlConnection.connect();
+            try(OutputStream os = urlConnection.getOutputStream())
+            {
+                os.write(out);
+            }
+            InputStream response = urlConnection.getInputStream();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void  DeleteHTTPData (String urlString,String json){
+        try{
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+
+            urlConnection.setRequestMethod("DELATE");
+            urlConnection.setDoOutput(true);
+
+            byte[] out = json.getBytes(StandardCharsets.UTF_8);
+            int lenght = out.length;
+
+            urlConnection.setFixedLengthStreamingMode(lenght);
+            urlConnection.setRequestProperty("Content-Type","application/json; charset=UTP-8");
+            urlConnection.connect();
+            try(OutputStream os = urlConnection.getOutputStream())
+            {
+                os.write(out);
+            }
+            InputStream response = urlConnection.getInputStream();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
